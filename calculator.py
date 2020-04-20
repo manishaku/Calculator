8f8bf15a9a959ab52a71704df0e347a56336d3b7
cpaluch <cpaluch@vt.edu>
2020-04-20 02:03:36 -0400

    triangle function plots a hardcoded triangle, not sure what parameters to use for triangle as there is a lot of variation with edges and anglesin geometry, also added a quit command to exit loop

--- calculator.py
+++ calculator.py
@@ -15,6 +15,7 @@
 
 import matplotlib.pyplot as plot
 import matplotlib.patches as patches
+from scipy import math
 
 mode = False
 
@@ -30,16 +31,24 @@ def parser():
         print("Select a shape: rectangle, triangle, circle, rhombus")
         shape = input('>')
         if shape == "rectangle":
-            l = int(input("what is the length"))
-            h = int(input("what is the height"))
-            rectangle(l, h)
+            inp = input("What is the length and height seperated by space: ")
+            inp = inp.split()
+            
+            rectangle(int(inp[0]), int(inp[1]))
         elif shape == "triangle":
-            triangle()
+            h = int(input("what is the height"))
+            b = int(input("what is the base length"))
+            e = int(input("what is one other edge length"))
+            triangle(h, b,e)
+
         elif shape == "circle":
             r = int(input("what is your radius"))
             circle(r)
         elif shape == "rhombus":
             rhombus()
+
+        elif shape == "quit": #Added a quit feature to get out of the loop
+            break
         else:
             print("Shape not available")
 
@@ -47,6 +56,7 @@ def parser():
 #Checks if mode == True and calls developerMode("rectangle")
 #Print the area
 #Call graph
+#prints the perimeter and area as well as plots the rectangle with the appropriate length and height
 def rectangle(length, height):
     perim = 2 * length + 2 * height
     area = length*height
@@ -55,7 +65,7 @@ def rectangle(length, height):
     fig = plot.figure()
     ax1 = fig.add_subplot(111, aspect='equal')
     ax1.add_patch(
-        patches.Rectangle((0, 0), length, height))
+        patches.Rectangle((3, 3), length, height))
     plot.ylim(0, 10)
     plot.xlim(0, 10)
     plot.show()
@@ -64,15 +74,25 @@ def rectangle(length, height):
 
 #Asks for the needed measurements
 #Checks if mode == True and calls developerMode("triangle")
-#Print the area
-#Call graph 
-def triangle():
-    pass
+#
+#Prints a triangle but is hardcoded currently, not sure of good parameters
+#as there are so many ways triangles can be formated in geomtry 
+def triangle(height, base, edge):
+    fig = plot.figure()
+    
+    tri = patches.Polygon([[0,0],[0, 5],[5, 0]], closed=True,fill=True)
+    sub = fig.add_subplot(111, aspect= 'equal')
+    sub.add_patch(tri)
+    plot.ylim(0, 10)
+    plot.xlim(0, 10)
+    sub.add_artist(tri)
+    plot.show()
 
 #Asks for the needed measurements
 #Checks if mode == True and calls developerMode("circle")
 #Print the area
-#Call graph   
+#Call graph
+#plots the circle based on the radius
 def circle(radius):
     fig = plot.figure()
     circle1 = patches.Circle((5, 5), radius, color = 'r')
@@ -103,6 +123,8 @@ def graph(info):
 #for each shape so should provide information about the code that follows
 def developerMode(shape):
     pass
+
+
 
 
 parser()
