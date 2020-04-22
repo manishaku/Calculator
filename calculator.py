2aafa73db351ad83e807738e53c49c875b34c28d
cpaluch <cpaluch@vt.edu>
2020-04-22 18:34:01 -0400

    style changes should be pretty good yoe is a list index error. Otherwise it is good

--- calculator.py
+++ calculator.py
@@ -19,7 +19,6 @@ from scipy import math
 import os
 import subprocess as sub
 
-devMode = False
 newFileCounter = 0
 fileList = []
 
@@ -54,38 +53,37 @@ def writePy():
 # Introduction info about the Calculator
 # Prompts the user to pick a shape
 def parser():
+
     global fileList
     print("Welcome to the Geometry Calculator.")
-    print("Would you like to enable developer mode? (Y/N)")
-    answer = input('>')
+    
+    print("CALC -> Would you like to enable developer mode? (Y/N)")
+    answer = input('CALC -> ')
     if answer == "Y":
         developerMode()
     while True:
-        print("Select a shape: rectangle, triangle, circle, rhombus")
-        shape = input('>')
+        print("CALC -> Select a shape: rectangle, triangle, circle, rhombus:")
+        shape = input('CALC -> ')
         if shape == "rectangle":
-            print("What is the length: ")
-            inp = input('>')
-            print("What is the height: ")
-            inp2 = input('>')
-            rectangle(int(inp), int(inp2))
+            rectangle()
         elif shape == "dev":
             developerMode()
         elif shape == "myFiles":
             for i in fileList:
-                print(i)
+                print("CALC -> " + i)
         elif shape == "triangle":
             triangle()
         elif shape == "circle":
-            print("what is your radius")
-            r = int(input('>'))
+            print("CALC -> Enter the radius: ")
+            r = int(input('CALC -> '))
             circle(r)
         elif shape == "rhombus":
             rhombus()
         else:
-            print("Shape not available")
-        print("Do you want to exit the calculator? (Y/N)")
-        v = input('>')
+            calcDev(shape)
+
+        print("CALC -> Do you want to exit the calculator? (Y/N)")
+        v = input('CALC -> ')
         if v == 'Y':
             break
 
@@ -93,11 +91,15 @@ def parser():
 #Print the area
 #Call graph
 #prints the perimeter and area as well as plots the rectangle with the appropriate length and height
-def rectangle(length, height):
+def rectangle():
+    print("CALC -> Enter the length: ")
+    length = int(input('CALC -> '))
+    print("CALC -> Enter the height: ")
+    height = int(input('CALC -> '))
     perim = 2 * length + 2 * height
     area = length*height
-    print("Perimeter = 2 * (length + width) = %.3f" %perim)
-    print("Area = length * width = %.3f" %area)
+    print("CALC -> Perimeter = 2 * (length + width) = %.3f" %perim)
+    print("CALC -> Area = length * width = %.3f" %area)
     fig = plot.figure()
     ax1 = fig.add_subplot(111, aspect='equal')
     ax1.add_patch(patches.Rectangle((3, 3), length, height))
@@ -108,14 +110,14 @@ def rectangle(length, height):
 #Asks for the needed measurements
 #Assumed Right Triangle
 def triangle():
-    print("Please enter the base length: ")
-    b = int(input(">")) 
-    print("Please enter the height: ")  
-    h = int(input(">")) 
+    print("CALC -> Please enter the base length: ")
+    b = int(input("CALC -> ")) 
+    print("CALC -> Please enter the height: ")  
+    h = int(input("CALC -> ")) 
     
     fig = plot.figure()
     area = (1/2)* b * h
-    print("Area: " + str(area))
+    print("CALC -> Area: " + str(area))
     p1 = (0, h)
     p2 = (0, 0)
     p3 = (b, 0)
@@ -134,8 +136,8 @@ def triangle():
 def circle(radius):
     area = math.pi*(int(radius)**2)
     perim = 2 * int(radius) * math.pi
-    print("Area = pi * radius * radius = %.3f" %area)
-    print("Circumference = 2 * pi * radius = %.3f" %perim)
+    print("CALC -> Area = pi * radius * radius = %.3f" %area)
+    print("CALC -> Circumference = 2 * pi * radius = %.3f" %perim)
     
     fig = plot.figure()
     circle1 = patches.Circle((int(radius), int(radius)), radius, color = 'r')
@@ -151,10 +153,10 @@ def circle(radius):
 #Call graph  
 def rhombus():
     fig = plot.figure()
-    p = int(input("Please enter the first diagonal length: "))
-    q = int(input("Please enter the second diagonal length: "))
+    p = int(input("CALC -> Please enter the first diagonal length: "))
+    q = int(input("CALC -> Please enter the second diagonal length: "))
     area = (p*q) / 2
-    print("Area: " + str(area))
+    print("CALC -> Area: " + str(area))
     p1 = (0, p/2)
     p2 = (q/2, p)
     p3 = (q, p/2)
@@ -173,38 +175,24 @@ def pointDist(p1, p2):
     
 #Calls files that were created in developer mode
 def calcDev(fileName):
+    global fileList
     fileInfo = fileName.split()
+    inList = False
+    for files in fileList:
+        if (fileInfo[0] == files):
+            inList =True;
+
+    if (not inList):
+        print("CALC -> That input is not available")
+        return
     command = ['python']
     command.extend(fileInfo)
     try:
         sub.check_call(command)
         
     except sub.CalledProcessError:
-        print("\nThere was an error in your input, Please try again")
+        print("CALC -> That input is not available")
 
-#Gets a list of information such as info = ["rectangle", 5, 8]   
-#or info = ["circle", 5]   
-#Uses the information to print a graph of the shape 
-def graph(info):
-    pass
-
-#Checks if the user is finished editing
-#Continues prompting the question until the Yes answer is returned
-def finishedEdit():
-    while (True):
-        inp = input("DEV -> Are you finished editing? (Y/N): ")
-        if (inp == "Y" or inp == 'y'):
-            print("DEV -> Your code will now run and check for errors. If it is error free your graph will be displayed!")
-            print("DEV -> If there are any errors in your code, they will be printed below and your plot will not display.\n")
-            return 0
-        elif (inp == "quit"):
-            return 2
-        elif (inp == "N"):
-            print("DEV -> Okay take your time!")
-            time.sleep(7)
-            return(finishedEdit())        
-        else:
-            print("DEV -> That is not a valid answer")
 
 #Checks if they are finished
 def finishedDev():
@@ -226,6 +214,7 @@ def addToList(fileName):
             inList = True
     if (not inList):
         fileList.append(fileName)
+
 #Removes from global list
 def removeFromList(fileName):
     global fileList
@@ -297,5 +286,14 @@ def developerMode():
         if (len(newFile) > 0):
             fileName = newFile
         
+        #gets user to input arguments and adds them to the command list
+        paramNum = int(input("DEV -> How many arguments does you function have? "))
+        commandArr = ['python', fileName]
+        if (paramNum  > 0):
+            params = input("DEV -> Please enter your function arguments seperated by spaces: ")
+            params = params.split()
+            commandArr.extend(params)
+        
+
 
 parser()
